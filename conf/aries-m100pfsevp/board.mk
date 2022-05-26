beb1f9ef0602df167c2facc3f79d90525aba3f86
Conor Dooley <conor.dooley@microchip.com>
2022-05-26 09:04:02 +0100

    linux: upgrade to linux4microchip+fpga-2022.05
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- conf/aries-m100pfsevp/board.mk
+++ conf/aries-m100pfsevp/board.mk
@@ -1,5 +1,5 @@
 HSS_SUPPORT ?= y
 HSS_TARGET ?= aries-m100pfsevp
 UBOOT_VERSION = 2020.10
-linux_defconfig := aries_m100pfsevp_defconfig
-linux_dtb := $(riscv_dtbdir)/aries/m100pfsevp-sdcard.dtb
+linux_defconfig := mpfs_defconfig
+linux_dtb := $(riscv_dtbdir)/microchip/mpfs-m100pfsevp-sdcard.dtb
